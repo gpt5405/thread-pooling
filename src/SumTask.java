de21da5d5293ace42d1d610b88b7e439fa69895b
Andrew <70240081+ABradbury823@users.noreply.github.com>
2025-10-15 21:58:19 -0400

    improved styling for output

--- src/SumTask.java
+++ src/SumTask.java
@@ -1,5 +1,7 @@
 public class SumTask implements Runnable {
 
+    private final String name;
+
     private final int[] data;
     private final int start;
     private final int end;
@@ -9,6 +11,9 @@ public class SumTask implements Runnable {
         this.data = data;
         this.start = start;
         this.end = end;
+        this.partialSum = 0;
+
+        this.name = "SumTask = " + formatNumber(start) + " - " + formatNumber(end);
     }
 
     @Override
@@ -18,10 +23,21 @@ public class SumTask implements Runnable {
             sum += data[i];
         }
         partialSum = sum;
-        System.out.println(Thread.currentThread().getName() + " completed: sum = " + sum);
+        System.out.printf("%-15s %-40s %-15s%n",
+            Thread.currentThread().getName(), 
+            name, 
+            "sum = " + formatNumber(sum));
     }
 
     public long getPartialSum() {
         return partialSum;
+    }
+
+    private String formatNumber(int number) {
+        return String.format("%,d", number);
+    }
+
+    private String formatNumber(long number) {
+        return String.format("%,d", number);
     }
 }
